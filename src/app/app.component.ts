8fb85bc78ad2d78ed366bf8fbfc947698c148203
Zachary K Whittenton <zwhitty11@gmail.com>
2026-08-04 00:30:03 -0500

    Build: Filled out remaining empty QOL pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
+
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+
+  standalone: true,
+
+  imports: [
+
+    RouterOutlet
+
+  ],
+
+  templateUrl:
+    './app.component.html',
+
+  styleUrl:
+    './app.component.css'
+
 })
 export class AppComponent {
-  title = 'austinsurfacepros';
+
+
 }
