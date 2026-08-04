80d197d7d04d1dbcea0cbdde05e96d839e174365
Zachary K Whittenton <zwhitty11@gmail.com>
2026-08-04 00:03:02 -0500

    Build: Added AI Generated Logo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SectionTitleComponent } from '../../shared/components/section-title/section-title.component';
+import { ServiceCardComponent } from '../../shared/components/service-card/service-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink,
+    SectionTitleComponent,
+    ServiceCardComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
