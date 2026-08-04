576609547bdda81a0621e80d1948c1ac636b79a7
Zachary K Whittenton <zwhitty11@gmail.com>
2026-08-03 23:54:22 -0500

    Build:Added Login/Register bones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,87 @@
 import { Routes } from '@angular/router';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component')
+          .then(m => m.HomeComponent)
+      },
+
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./pages/about/about.component')
+          .then(m => m.AboutComponent)
+      },
+
+      {
+        path: 'services',
+        loadComponent: () =>
+          import('./pages/services/services.component')
+          .then(m => m.ServicesComponent)
+      },
+
+      {
+        path: 'gallery',
+        loadComponent: () =>
+          import('./pages/gallery/gallery.component')
+          .then(m => m.GalleryComponent)
+      },
+
+      {
+        path: 'resources',
+        loadComponent: () =>
+          import('./pages/resources/resources.component')
+          .then(m => m.ResourcesComponent)
+      },
+
+      {
+        path: 'contact',
+        loadComponent: () =>
+          import('./pages/contact/contact.component')
+          .then(m => m.ContactComponent)
+      },
+
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./pages/login/login.component')
+          .then(m => m.LoginComponent)
+      },
+
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./pages/register/register.component')
+          .then(m => m.RegisterComponent)
+      },
+
+      {
+        path: 'privacy-policy',
+        loadComponent: () =>
+          import('./pages/privacy-policy/privacy-policy.component')
+          .then(m => m.PrivacyPolicyComponent)
+      },
+
+      {
+        path: 'terms',
+        loadComponent: () =>
+          import('./pages/terms/terms.component')
+          .then(m => m.TermsComponent)
+      }
+
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
