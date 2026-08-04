67a43c8bc5170a32279b1fc0f6c9004b7ff52527
Zachary K Whittenton <zwhitty11@gmail.com>
2026-08-03 23:57:36 -0500

    Build: Shared Folder Components

---
+++ src/app/shared/components/service-card/service-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+ selector:'app-service-card',
+ templateUrl:'./service-card.component.html',
+ styleUrl:'./service-card.component.css'
+})
+export class ServiceCardComponent {
+
+ @Input()
+ title='';
+
+ @Input()
+ description='';
+
+ @Input()
+ image='';
+
+}
