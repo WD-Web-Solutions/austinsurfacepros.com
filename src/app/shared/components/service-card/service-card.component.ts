32e40a51fe47aee47ab8baf48501f8300e102a58
Zachary K Whittenton <zwhitty11@gmail.com>
2026-08-04 00:07:50 -0500

    Build: Updated CSS on Shared components

--- src/app/shared/components/service-card/service-card.component.ts
+++ src/app/shared/components/service-card/service-card.component.ts
@@ -1,19 +1,20 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
- selector:'app-service-card',
- templateUrl:'./service-card.component.html',
- styleUrl:'./service-card.component.css'
+  selector: 'app-service-card',
+  imports: [],
+  templateUrl: './service-card.component.html',
+  styleUrl: './service-card.component.css'
 })
 export class ServiceCardComponent {
 
- @Input()
- title='';
+  @Input()
+  title = '';
 
- @Input()
- description='';
+  @Input()
+  description = '';
 
- @Input()
- image='';
+  @Input()
+  image = '';
 
 }
