45f0864094cf5bd0046e1a072e7afa604901867e
derek-dreibrodt <ddreibrodt1@gmail.com>
2026-08-09 22:17:01 -0500

    feat: refine gallery and navbar aesthetics

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -81,6 +81,26 @@ export const routes: Routes = [
 
 
       {
+        path: 'login',
+
+        loadComponent: () =>
+          import('./pages/login/login.component')
+            .then(m => m.LoginComponent)
+
+      },
+
+
+      {
+        path: 'register',
+
+        loadComponent: () =>
+          import('./pages/register/register.component')
+            .then(m => m.RegisterComponent)
+
+      },
+
+
+      {
         path: 'privacy-policy',
 
         loadComponent: () =>
